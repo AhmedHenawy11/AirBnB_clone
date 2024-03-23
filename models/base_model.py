0b5fed88c712a970727cef2270c6dc8862db0356
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-24 00:56:02 +0200

    synchronous gibberish

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a base class to be inherited for all models in our project"""
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        """ Instantiation of each object created """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Representational method of the instance """
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def save(self):
+        """ Method that updates the public instance attribute updated_at with the current datetime """
+        self.updated_at = datetime.now()
+
