212e6b61c1de7696af3747864b884e2089a03e3e
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-25 04:21:31 +0200

    Ramayana's benching

--- models/base_model.py
+++ models/base_model.py
@@ -27,11 +27,6 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
     def __str__(self):
         """ Representational method of the instance """
         return f"[{type(self).__name__}] ({self.id}) <{self.__dict__}>"
