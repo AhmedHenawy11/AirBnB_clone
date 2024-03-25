04f05c21cc7315cce0d44d05fd557cf79c6f0858
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-25 02:21:42 +0200

    miserly hazing's

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,22 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        """ Instantiation of each object created """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """A base class for all hbnb models"""
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """ Representational method of the instance """
