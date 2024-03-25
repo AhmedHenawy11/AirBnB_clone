003f7bf53b33fcddf0f5345215fab53b87a5d3e4
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-25 02:40:01 +0200

    ulcerate fancier

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
+"""This module defines a base class to be inherited for all models in our project"""
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+    """A base class for all  models"""
+    def __init__(self):
+        """ Instantiation of each object created """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """ Representational method of the instance """
+        return f"[{type(self).__name__}] ({self.id}) <{self.__dict__}>"
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """ Method that updates the public instance attribute updated_at with the current datetime """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """ returns a dictionary containing all keys/values of __dict__ of the instance """
+        dict = self.__dict__
+        dict.update({'__class__': type(self).__name__ })
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        return dict
