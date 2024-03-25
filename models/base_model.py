26e8213d76aa4c27d9b71c9891ed665c1f9e8934
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-25 02:19:00 +0200

    tonsorial task

--- models/base_model.py
+++ models/base_model.py
@@ -21,9 +21,11 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance """
-        dict = self.__dict__
-        dict.update({'__class__': type(self).__name__ })
-        dict['created_at'] = self.created_at.isoformat()
-        dict['updated_at'] = self.updated_at.isoformat()
-        return dict
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
