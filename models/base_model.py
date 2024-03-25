b8fa20e6ef5c3d34264dc8e971ca8984d7416389
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-03-25 02:18:27 +0200

    capitalist sclerotic

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self):
         """ Instantiation of each object created """
@@ -19,3 +20,10 @@ class BaseModel:
         """ Method that updates the public instance attribute updated_at with the current datetime """
         self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ of the instance """
+        dict = self.__dict__
+        dict.update({'__class__': type(self).__name__ })
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        return dict
