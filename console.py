0348e6d9842fe982700445f2c2320b3216802ab4
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-04-01 08:24:05 +0200

    euphoria's Ascella's

--- console.py
+++ console.py
@@ -8,12 +8,23 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        return True
+    def do_quit(self, command):
+        """ Method to exit the HBNB console"""
+        exit()
+
+    def help_quit(self):
+        """ Prints the help documentation for quit  """
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        exit()
 
     def help_EOF(self):
         """ Prints the help documentation for EOF """
         print("Exits the program without formatting\n")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
