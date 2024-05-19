fb4371ed0404ab2815e7a25bd00ff9039b43b6ed
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-05-20 02:53:42 +0300

    unsaid favorable

---
+++ models/engine/file_storage.py
@@ -0,0 +1,12 @@
+# #!/usr/bin/python3
+# """ a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances """
+#
+# import json
+#
+#
+# class FileStorage:
+#     """a class  that serializes instances to a JSON file and deserializes JSON file to instances"""
+#     __file_path = "file.json"
+#     __objects = {}
+#
+#     def all(self):
