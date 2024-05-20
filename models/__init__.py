87037157e6c127c850eb78544d1cd4ec0b652207
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-05-20 16:39:34 +0300

    Barry's floorboard

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# #!/usr/bin/python3
-# """the module __init__.py"""
-# from models.engine import file_storage
-#
-# storage = file_storage.FileStorage()
-# storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -7,22 +7,7 @@ from datetime import datetime
 class BaseModel:
     """A base class for all  models"""
 
-    def __init__(self, *args, **kwargs):
-        """ Instantiation of each object created """
-        # date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        # if kwargs:
-        #     for key, value in kwargs.items():
-        #         if "created_at" == key:
-        #             self.created_at = datetime.strptime(kwargs["created_at"],
-        #                                                 date_format)
-        #         elif "updated_at" == key:
-        #             self.updated_at = datetime.strptime(kwargs["updated_at"],
-        #                                                 date_format)
-        #         elif "__class__" == key:
-        #             pass
-        #         else:
-        #             setattr(self, key, value)
-        # else:
+    def __init__(self):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -39,8 +24,8 @@ class BaseModel:
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
         dicto = self.__dict__
         dicto.update({'__class__': type(self).__name__})
-        dict['created_at'] = self.created_at.isoformat()
-        dict['updated_at'] = self.updated_at.isoformat()
+        dicto['created_at'] = datetime.isoformat()
+        dicto['updated_at'] = datetime.isoformat()
 
         return dicto
 
