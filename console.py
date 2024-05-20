f7b894931109e05614eeeae69be6beb0ed123cfb
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-05-20 16:26:04 +0300

    pierced shipboard's

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
-
+from models.base_model import BaseModel
+from models.__init__ import storage
 
 class HBNBCommand(cmd.Cmd):
     """ CMD Class """
 
     prompt = "(hbnb) "
+
+    classes = {
+               'BaseModel': BaseModel,
+    }
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
@@ -28,6 +33,20 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
+
+    def do_create(self, args):
+        """ Create an object of any class"""
+        if not args:
+            print("** class name missing **")
+            return
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
